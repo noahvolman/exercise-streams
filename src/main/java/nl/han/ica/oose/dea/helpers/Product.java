c4dcea52e239e1a83201900748eba26af618ee29
Bart van der Wal <bart.vanderwal@han.nl>
2020-02-12 23:37:43 +0100

    Extra opgave met filter en sum of reduce.

--- src/main/java/nl/han/ica/oose/dea/helpers/Product.java
+++ src/main/java/nl/han/ica/oose/dea/helpers/Product.java
@@ -1,12 +1,20 @@
 package nl.han.ica.oose.dea.helpers;
 
+import static nl.han.ica.oose.dea.helpers.ProductCategory.GADGETS;
+
 public class Product {
     private String name;
     private int price;
+    private ProductCategory category;
 
     public Product(String name, int price) {
+        this(name, price, GADGETS);
+    }
+
+    public Product(String name, int price, ProductCategory category) {
         this.name = name;
         this.price = price;
+        this.category = category;
     }
 
     public String getName() {
@@ -16,4 +24,7 @@ public class Product {
     public int getPrice() {
         return price;
     }
+
+    public ProductCategory getCategory() { return category; }
+
 }
