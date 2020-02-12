2b590f49173d2afedccd1741b75d58901d261846
Bart van der Wal <bart.vanderwal@han.nl>
2020-02-12 23:45:30 +0100

    Extra opgave met filter en sum of reduce.

--- src/main/java/nl/han/ica/oose/dea/Streams.java
+++ src/main/java/nl/han/ica/oose/dea/Streams.java
@@ -2,6 +2,7 @@ package nl.han.ica.oose.dea;
 
 
 import nl.han.ica.oose.dea.helpers.Product;
+import nl.han.ica.oose.dea.helpers.ProductCategory;
 
 import java.util.Comparator;
 import java.util.List;
@@ -9,43 +10,34 @@ import java.util.stream.Collectors;
 
 class Streams {
 
-    List<String> filterStringsLongerThanThreeCharacters(List<String> input) {
-        var result = input.stream().filter((element) -> element.length() < 3).collect(Collectors.toList());
+    List<String> filterStringsShorterThanThreeCharacters(List<String> input) {
+        var result = input; // implement
 
-        return result;
+        return result.stream().filter(string -> string.length()<3).collect(Collectors.toList());
     }
 
-    List<String> filterStringsThanContainOnlyNumerals(List<String> input) {
-        var result = input.stream().filter((element) -> isInteger(element)).collect(Collectors.toList());
+    List<String> filterStringsThatContainOnlyNumerals(List<String> input) {
+        var result = input; // implement
 
-        return result;
+        return result.stream().filter(string -> string.matches("[\\d]+")).collect(Collectors.toList());
     }
 
     String findShortestString(List<String> input) {
-        var shortestString = input.stream().min(Comparator.comparingInt(String::length));
-
-        return shortestString.orElse("");
+        return input.stream().sorted().findFirst().orElse("");
     }
-
-
-    private boolean isInteger(String element) {
-        try {
-            Integer.parseInt(element);
-        } catch (NumberFormatException ex) {
-            return false;
-        }
-        return true;
-    }
-
 
     String createAFullSentenceFromTheList(List<String> input) {
-        var fullSentence = input.stream().reduce((s1, s2) -> s1 + " " + s2);
-
-        return fullSentence.orElse("");
+        return input.stream().collect(Collectors.joining(" "));
     }
 
     int calculateTotalCostOfAllProducts(List<Product> products) {
-        var totalCost = products.stream().mapToInt(Product::getPrice).reduce(0, (p1, p2) -> p1 + p2);
-        return totalCost;
+        return products.stream().map(product -> product.getPrice())
+                .reduce((prev, next) -> prev+next).orElse(0);
+    }
+
+    int calculateTotalCostOfAllGadgets(List<Product> products) {
+        return products.stream().filter(product -> product.getCategory()== ProductCategory.GADGETS)
+                .map(product -> product.getPrice())
+                .mapToInt(Integer::intValue).sum();
     }
 }
