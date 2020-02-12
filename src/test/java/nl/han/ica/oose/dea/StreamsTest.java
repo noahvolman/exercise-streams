13791faf115265224ff401e0771a57a50da400ae
Bart van der Wal <bart.vanderwal@han.nl>
2020-02-12 23:33:45 +0100

    Extra opgave met filter en sum of reduce.

--- src/test/java/nl/han/ica/oose/dea/StreamsTest.java
+++ src/test/java/nl/han/ica/oose/dea/StreamsTest.java
@@ -1,6 +1,7 @@
 package nl.han.ica.oose.dea;
 
 import nl.han.ica.oose.dea.helpers.Product;
+import nl.han.ica.oose.dea.helpers.ProductCategory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,80 +14,112 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 
 class StreamsTest {
-    private Streams streams;
+    private Streams sut;
 
     @BeforeEach
     void setup() {
-        streams = new Streams();
+        sut = new Streams();
     }
 
     @Test
-    void testRemoveStringsWithMoreThanThreeCharacters() {
+    void test1RemoveStringsWithMoreThanThreeCharacters() {
         // Setup
         List<String> input = asList("Welcome", "to", "Java", "8", "Streams");
 
         // Test
-        List<String> filteredStrings = streams.filterStringsLongerThanThreeCharacters(input);
+        List<String> filteredStrings = sut.filterStringsShorterThanThreeCharacters(input);
 
         // Verify
         assertThat(filteredStrings, contains("to", "8"));
     }
 
     @Test
-    void testRemoveNonNumeralStrings() {
+    void test2RemoveNonNumeralStrings() {
         // Setup
         List<String> input = asList("Welcome", "to", "Java", "8", "Streams");
 
         // Test
-        List<String> filteredStrings = streams.filterStringsThanContainOnlyNumerals(input);
+        List<String> filteredStrings = sut.filterStringsThatContainOnlyNumerals(input);
 
         // Verify
         assertThat(filteredStrings, contains("8"));
     }
 
     @Test
-    void testFindShortestString() {
+    void test3AFindShortestString() {
         // Setup
         List<String> input = asList("Welcome", "to", "Java", "8", "Streams");
 
         // Test
-        String foundString = streams.findShortestString(input);
+        String foundString = sut.findShortestString(input);
 
         // Verify
         Assertions.assertEquals("8", foundString);
     }
 
     @Test
-    void testCreateAFullSentenceFromTheList() {
+    void test3BFindShortestString() {
+        // Setup
+        List<String> input = asList(new String[]{});
+
+        // Test
+        String foundString = sut.findShortestString(input);
+
+        // Verify
+        Assertions.assertEquals("", foundString);
+    }
+
+    @Test
+    void test4CreateAFullSentenceFromTheList() {
         // Setup
         List<String> input = asList("Welcome", "to", "Java", "8", "Streams");
 
         // Test
-        String foundString = streams.createAFullSentenceFromTheList(input);
+        String foundString = sut.createAFullSentenceFromTheList(input);
 
         // Verify
         Assertions.assertEquals("Welcome to Java 8 Streams", foundString);
     }
 
     @Test
-    void testCalculateTotalCostOfAllProducts() {
+    void test5ACalculateTotalCostOfAllProducts() {
         // Setup
+        var input = initialiseTestProductSet();
+
+        // Test
+        int totalValue = sut.calculateTotalCostOfAllProducts(input);
+
+        // Verify
+        Assertions.assertEquals(7286, totalValue);
+    }
+
+    @Test
+    void test6CalculateTotalCostOfAllGadgets() {
+        // Setup
+        var input = initialiseTestProductSet();
+
+        // Test
+        int totalValue = sut.calculateTotalCostOfAllGadgets(input);
+
+        // Verify
+        Assertions.assertEquals(3886, totalValue);
+    }
+
+    private List<Product> initialiseTestProductSet() {
+        List<Product> input;
+
         final int PRICE_TV = 1200;
         final int PRICE_LAPTOP = 2300;
         final int PRICE_TABLET = 149;
         final int PRICE_PHONE = 237;
+        final int PRICE_TABLE = 3400;
 
         var tv = new Product("TV", PRICE_TV);
         var laptop = new Product("Laptop", PRICE_LAPTOP);
         var tablet = new Product("tablet", PRICE_TABLET);
         var phone = new Product("phone", PRICE_PHONE);
+        var table = new Product("table", PRICE_TABLE, ProductCategory.FURNITURE);
 
-        List<Product> input = asList(tv, laptop, tablet, phone);
-
-        // Test
-        int totalValue = streams.calculateTotalCostOfAllProducts(input);
-
-        // Verify
-        Assertions.assertEquals(PRICE_TV + PRICE_LAPTOP + PRICE_TABLET + PRICE_PHONE, totalValue);
+        return asList(tv, laptop, tablet, phone, table);
     }
 }
